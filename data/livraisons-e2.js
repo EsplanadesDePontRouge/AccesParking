a11d7dea1b8afaed0f6a9add482f5ffcb48834de
theolelievre7 <theo.lelievre@wincasa.ch>
2025-09-18 12:16:06 +0200

    Update livraisons-e2.js

--- data/livraisons-e2.js
+++ data/livraisons-e2.js
@@ -6,23 +6,23 @@ const map = L.map('map', {
 const image = 'assets/EPR_SS1.png';
 const imageSize = [1007, 1215];
 const bounds = [[0, 0], imageSize];
-
 L.imageOverlay(image, bounds).addTo(map);
 map.fitBounds(bounds);
 
-// Cheminement E2
-const chemin = [
-  [774, 812],
-  [776, 887],
-  [725, 886],
-  [725, 690],
-  [530, 690],
-  [530, 990],
-  [475, 990]
-];
+function corriger(x, y) {
+  return [1007 - y, x];
+}
 
+// 🔵 Cheminement E2
+const chemin = [
+  corriger(990, 520),
+  corriger(990, 471),
+  corriger(690, 468),
+  corriger(680, 273),
+  corriger(887, 269)
+];
 L.polyline(chemin, { color: '#0078D4', weight: 4 }).addTo(map);
 
-// Points E2
-L.marker([774, 812]).addTo(map).bindPopup("Livraison E2");
-L.marker([776, 887]).addTo(map).bindPopup("Livraison E2");
+// 📍 Pings E2
+L.marker(corriger(811, 224)).addTo(map).bindTooltip("L7", { permanent: true, direction: "top" });
+L.marker(corriger(886, 222)).addTo(map).bindTooltip("L8", { permanent: true, direction: "top" });
