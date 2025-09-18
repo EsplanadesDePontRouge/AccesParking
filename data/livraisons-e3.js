06cd41073365d60c6329c465947b273299d362d2
theolelievre7 <theo.lelievre@wincasa.ch>
2025-09-18 11:23:02 +0200

    Update livraisons-e3.js

--- data/livraisons-e3.js
+++ data/livraisons-e3.js
@@ -6,24 +6,24 @@ const map = L.map('map', {
 const image = 'assets/EPR_SS1.png';
 const imageSize = [1007, 1215];
 const bounds = [[0, 0], imageSize];
+
 L.imageOverlay(image, bounds).addTo(map);
 map.fitBounds(bounds);
 
 // Cheminement E3
 const chemin = [
-  [1007 - 799, 250],
-  [1007 - 799, 533],
-  [1007 - 990, 533],
-  [1007 - 990, 477]
+  [328, 849],
+  [296, 849],
+  [251, 849],
+  [250, 799],
+  [533, 799],
+  [533, 990],
+  [477, 990]
 ];
 
-const points = [
-  [1007 - 849, 328],
-  [1007 - 849, 296],
-  [1007 - 849, 251]
-];
+L.polyline(chemin, { color: '#28A745', weight: 4 }).addTo(map);
 
-L.polyline(chemin, { color: '#28A745' }).addTo(map);
-points.forEach(p => {
-  L.marker(p).addTo(map).bindPopup("Livraison E3");
-});
+// Points E3
+L.marker([328, 849]).addTo(map).bindPopup("Livraison E3");
+L.marker([296, 849]).addTo(map).bindPopup("Livraison E3");
+L.marker([251, 849]).addTo(map).bindPopup("Livraison E3");
