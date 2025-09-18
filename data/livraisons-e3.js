ac2f9bf6aada90683bf72e9aa599747ddb0e5a34
theolelievre7 <theo.lelievre@wincasa.ch>
2025-09-18 12:16:59 +0200

    Update livraisons-e3.js

--- data/livraisons-e3.js
+++ data/livraisons-e3.js
@@ -13,16 +13,16 @@ function corriger(x, y) {
   return [1007 - y, x];
 }
 
-// 🔵 Cheminement E2
+// 🟢 Cheminement E3
 const chemin = [
   corriger(990, 520),
   corriger(990, 471),
-  corriger(690, 468),
-  corriger(680, 273),
-  corriger(887, 269)
+  corriger(800, 472),
+  corriger(806, 750)
 ];
-L.polyline(chemin, { color: '#0078D4', weight: 4 }).addTo(map);
+L.polyline(chemin, { color: '#28A745', weight: 4 }).addTo(map);
 
-// 📍 Pings E2
-L.marker(corriger(811, 224)).addTo(map).bindTooltip("L7", { permanent: true, direction: "top" });
-L.marker(corriger(886, 222)).addTo(map).bindTooltip("L8", { permanent: true, direction: "top" });
+// 📍 Pings E3
+L.marker(corriger(849, 673)).addTo(map).bindTooltip("L1", { permanent: true, direction: "top" });
+L.marker(corriger(849, 704)).addTo(map).bindTooltip("L2", { permanent: true, direction: "top" });
+L.marker(corriger(850, 746)).addTo(map).bindTooltip("L3", { permanent: true, direction: "top" });
