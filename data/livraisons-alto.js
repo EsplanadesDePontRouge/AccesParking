bb88ba45d29930bdab90ebe1ecb953318fc0c557
theolelievre7 <theo.lelievre@wincasa.ch>
2025-09-18 12:17:10 +0200

    Update livraisons-alto.js

--- data/livraisons-alto.js
+++ data/livraisons-alto.js
@@ -13,16 +13,17 @@ function corriger(x, y) {
   return [1007 - y, x];
 }
 
-// 🔵 Cheminement E2
+// 🟠 Cheminement ALTO
 const chemin = [
   corriger(990, 520),
   corriger(990, 471),
   corriger(690, 468),
   corriger(680, 273),
-  corriger(887, 269)
+  corriger(292, 275),
+  corriger(292, 550)
 ];
-L.polyline(chemin, { color: '#0078D4', weight: 4 }).addTo(map);
+L.polyline(chemin, { color: '#FF8800', weight: 4 }).addTo(map);
 
-// 📍 Pings E2
-L.marker(corriger(811, 224)).addTo(map).bindTooltip("L7", { permanent: true, direction: "top" });
-L.marker(corriger(886, 222)).addTo(map).bindTooltip("L8", { permanent: true, direction: "top" });
+// 📍 Pings ALTO
+L.marker(corriger(190, 278)).addTo(map).bindTooltip("L4", { permanent: true, direction: "top" });
+L.marker(corriger(295, 606)).addTo(map).bindTooltip("Livraisons ALTO", { permanent: true, direction: "top" });
